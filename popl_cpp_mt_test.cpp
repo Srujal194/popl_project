40b7d65179cf19f5b00abcc55f9f915765ac6a3b
Srujal194 <102753313+Srujal194@users.noreply.github.com>
2023-11-20 01:43:58 +0530

    Update popl_cpp_mt_test.cpp
    Changed to make file inputs

--- popl_cpp_mt_test.cpp
+++ popl_cpp_mt_test.cpp
@@ -1,28 +1,51 @@
 #include <iostream>
-#include <vector>
+#include <fstream>
 #include <thread>
+#include <vector>
 
-void reader(const std::vector<int>& data) {
-    for (int item : data) {
-        std::cout << "Read: " << item << std::endl;
+using namespace std;
+
+void reader(const string& file_path, vector<string>& content) {
+    ifstream input_file(file_path);
+    if (!input_file.is_open()) {
+        cerr << "Failed to open input file" << endl;
+        return;
     }
+
+    string line;
+    while (getline(input_file, line)) {
+        content.push_back(line);
+        cout << "Read: " << line << endl;
+    }
+    input_file.close();
+
+    cout << "Finished reading from file" << endl;
 }
 
-void writer(const std::vector<int>& data) {
-    for (int item : data) {
-        std::cout << "Write: " << item * 2 << std::endl;
+void writer(const string& file_path, const vector<string>& content) {
+    ofstream output_file(file_path);
+    if (!output_file.is_open()) {
+        cerr << "Failed to create output file" << endl;
+        return;
     }
+
+    for (const string& line : content) {
+        output_file << "Write: " << line << endl;
+    }
+    output_file.close();
+
+    cout << "Finished writing to file" << endl;
 }
 
 int main() {
-    std::vector<int> data = {1, 2, 3, 4, 5};
+    string input_file_path = "input.txt";
+    string output_file_path = "output.txt";
+    vector<string> file_content;
 
     // Create threads for reading and writing
-    std::thread reader_thread(reader, data);
-    std::thread writer_thread(writer, data);
-
-    // Wait for both threads to finish
+    thread reader_thread(reader, input_file_path, ref(file_content));
     reader_thread.join();
+    thread writer_thread(writer, output_file_path, cref(file_content));
     writer_thread.join();
 
     return 0;
